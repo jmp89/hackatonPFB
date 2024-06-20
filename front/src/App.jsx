0a7fd3d5117e96fc969bf41ad8277798e78793ef
Juan Ignacio Macias Perez <juanmaciasperez89@gmail.com>
2024-06-20 18:56:11 +0100

    Added front template VITE + TAW

---
+++ front/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-center font-bold underline">
+        Inicio del proyecto
+      </h1>
+    </>
+  );
+}
+
+export default App;

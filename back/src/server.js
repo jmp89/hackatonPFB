848b625cdef950eaece5065d8843431c134b7ac0
Adrián Conde Pérez <acp641991@gmail.com>
2024-06-26 09:57:53 +0200

    Middleware CORS creado

--- back/src/server.js
+++ back/src/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 
 const server = express();
 
 server.use(morgan("dev"));
+
+server.use(cors());
 
 // Llamado a rutas
 server.get("/", (req, res) => {

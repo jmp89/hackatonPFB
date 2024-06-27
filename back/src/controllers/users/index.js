3ddd74577b3b04a6205d195beaf2ca7245325134
George Nicu <gnicuu@gmail.com>
2024-06-27 15:54:33 +0200

    createEventAdmin

--- back/src/controllers/users/index.js
+++ back/src/controllers/users/index.js
@@ -1,6 +1,8 @@
 import validateUserController from "./validateUserController.js";
 import createEventAdminController from "./createEventAdmin.js";
+import eventRegistrationController from "./eventRegistrationController.js"
 
 export { validateUserController,
-        createEventAdminController
+        createEventAdminController,
+        eventRegistrationController
  };
--- back/src/routes/userRouter.js
+++ back/src/routes/userRouter.js
@@ -5,10 +5,12 @@ import {
   eventRegistrationController,
 } from "../controllers/users/index.js";
 import {createEventAdminController} from "../controllers/users/index.js";
-import authAdmin from "../middleware/authAdmin.js";
+import authAdmin from "../middlewares/authAdmin.js";
 
 const router = express.Router();
 
 router.post("/validate", validateUserController);
+
+router.post("/event", authAdmin, createEventAdminController)
 
 export default router;

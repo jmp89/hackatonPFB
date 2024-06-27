f42f0284496ddcea99ed737f7facc0a744073f7a
Samuel Lamas <samusada95@gmail.com>
2024-06-27 11:20:37 +0200

    eliminación del urlencodedParser, fallaba y no lo necesitamos

--- back/src/middlewares/bodyParser.js
+++ back/src/middlewares/bodyParser.js
@@ -18,24 +18,3 @@ export const jsonParser = (req, res, next) => {
         next();
     }
 };
-
-// Middleware para parsear datos URL-encoded
-export const urlencodedParser = (req, res, next) => {
-    if (req.headers['content-type'] === 'application/x-www-form-urlencoded') {
-        let data = '';
-        req.on('data', chunk => {
-            data += chunk;
-        });
-
-        req.on('end', () => {
-            req.body = data.split('&').reduce((acc, pair) => {
-                const [key, value] = pair.split('=');
-                acc[decodeURIComponent(key)] = decodeURIComponent(value.replace(/\+/g, ' '));
-                return acc;
-            }, {});
-            next();
-        });
-    } else {
-        next();
-    }
-};

a818a0d8feff4e02d2cf67ea7e16af9a444afe15
Adrián Conde Pérez <acp641991@gmail.com>
2024-06-27 19:55:28 +0200

    Endpoint busqueda-filtrado-ordenado de Eventos

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,11 +1,12 @@
 import express from "express";
 
+import entriesRouter from "./entriesRouter.js";
 import userRouter from "./userRouter.js";
 
 const router = express.Router();
 
+router.use(entriesRouter);
 router.use(userRouter);
-
 
 // Ruta de ejemplo para hacer test del middleware de bodyParser
 router.post('/testParser', (req, res) => {

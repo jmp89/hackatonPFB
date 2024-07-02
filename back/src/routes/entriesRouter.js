9c17dd8006df61b1d5c70ebc6d70bca290433654
Adrián Conde Pérez <acp641991@gmail.com>
2024-07-02 13:03:43 +0200

    Endpoint Creacion Eventos Solo Admin

--- back/src/routes/entriesRouter.js
+++ back/src/routes/entriesRouter.js
@@ -3,15 +3,16 @@ import listEventsController from "../controllers/entries/listEventsController.js
 import listEventDetailsController from "../controllers/entries/listEventDetailsController.js";
 import validateEventParticipationController from "../controllers/entries/validateEventParticipationController.js";
 import eventRegistrationController from "../controllers/entries/eventRegistrationController.js";
-import { createEventAdminController } from "../controllers/users/index.js";
-import listEventTematicasController from "../controllers/entries/listEventTematicasController.js";
+import createEventAdminController from "../controllers/entries/createEventAdminController.js";
+import authAdminService from "../services/entries/authAdminService.js";
+import listEventTematicasController from "../controllers/entries/listEventTematicasController.js"
 
 const router = express.Router();
 
 router
   .get("/entries/search", listEventsController)
   .get("/entries/:eventID", listEventDetailsController)
-  //   .post("/event", authAdmin, createEventAdminController)
+  .post("/event", authAdminService, createEventAdminController)
   .post("/event/register", eventRegistrationController)
   .get("/event/confirm/:eventCode", validateEventParticipationController)
   .get("/event/tematicas", listEventTematicasController);

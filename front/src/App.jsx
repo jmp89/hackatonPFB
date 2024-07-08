314453c8d25d35394d8182577d6bd96043d76cfd
Juan Ignacio Macias Perez <juanmaciasperez89@gmail.com>
2024-07-08 18:58:06 +0100

    Añadido pages/Home.jsx como ruta principal

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,9 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
     return (
         <Routes>
-            {/* <Route path="/" element={<Home />} /> */}
+            <Route path="/" element={<Home />} />
 
             {/* <Route path="/site1" element={<Site1 />} /> */}
 

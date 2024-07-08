751dd4ba09f3379e731ad9e4bbbf2451ef27df27
Juan Ignacio Macias Perez <juanmaciasperez89@gmail.com>
2024-07-08 18:50:53 +0100

    Añadida dependencia react-router-dom, y carpetas de estructura: components, context, pages y utils

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,11 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl text-center font-bold underline">
-        Inicio del proyecto
-      </h1>
-    </>
-  );
+    return (
+        <Routes>
+            {/* <Route path="/" element={<Home />} /> */}
+
+            {/* <Route path="/site1" element={<Site1 />} /> */}
+
+            {/* <Route path="/site2" element={<Site2 />} /> */}
+
+            {/* <Route path="/site3" element={<Site3 />} /> */}
+        </Routes>
+    );
 }
 
 export default App;

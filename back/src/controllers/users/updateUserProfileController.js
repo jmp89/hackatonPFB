62be5410c7bee982a4a349e7ac078fd44d509fe3
George Nicu <gnicuu@gmail.com>
2024-07-09 13:03:29 +0200

    perfil actualizado

--- back/src/controllers/users/updateUserProfileController.js
+++ back/src/controllers/users/updateUserProfileController.js
@@ -1,18 +1,17 @@
-
-import updateUserProfile from "../../services/users/updateUserProfile.js"
+import updateUserProfile from "../../services/users/updateUserProfile.js";
 
 const updateUserProfileController = async (req, res) => {
     const userId = req.params.id;
-    const { personal_info } = req.body;
+    const { name, email, personal_info } = req.body;
 
     try {
-        await updateUserProfile(userId, personal_info);
+        console.log(`Updating profile for user ${userId} with name: ${name}, email: ${email}, personal_info: ${personal_info}`);
+
+        await updateUserProfile(userId, name, email, personal_info);
         res.status(200).send('Profile updated successfully');
     } catch (err) {
         res.status(500).send(err.message);
     }
 };
-
-
 
 export default updateUserProfileController;
--- back/src/services/users/updateUserProfile.js
+++ back/src/services/users/updateUserProfile.js
@@ -1,11 +1,12 @@
 import getPool from "../../database/getPool.js";
 
-const updateUserProfile = async (userId, personalInfo) => {
+const updateUserProfile = async (userId, name, email, personalInfo) => {
     try {
+
         const pool = await getPool();
         const [results] = await pool.query(
-            'UPDATE users SET personal_info = ? WHERE id = ?',
-            [personalInfo, userId]
+            'UPDATE users SET name = ?, email = ?, personal_info = ? WHERE id = ?',
+            [name, email, personalInfo, userId]
         );
         return results;
     } catch (error) {

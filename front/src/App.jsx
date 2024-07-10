4e0698b18bc764559d802cde9be6b41568fc4039
Samuel Lamas <samusada95@gmail.com>
2024-07-10 11:37:21 +0200

    Creación de formulario de registro

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,20 +2,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import AdminPage from './pages/AdminPage';
 import Home from './pages/Home';
+import RegisterFormPage from './pages/RegisterFormPage';
 
 const App = () => {
   return (
     <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/admin" element={<AdminPage />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/register" element={<RegisterFormPage />} />
+      </Routes>
     </AuthProvider>
   );
 };
 
 export default App;
-
-

c6cd76bb97dcc13392fcb92c97d0ad14f1cf6837
oscar <ordc120@gmail.com>
2024-07-10 13:27:49 +0200

    conflito en git resuelto

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import AdminPage from './pages/AdminPage';
 import Home from './pages/Home';
+import RegisterFormPage from './pages/RegisterFormPage';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/admin/:eventId?" element={<AdminPage />} />
+      <Route path="/register" element={<RegisterFormPage />} />
     </Routes>
   );
 };

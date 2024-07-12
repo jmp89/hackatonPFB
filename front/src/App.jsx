2e538ce8a45b026b2ae09621003e21dcea643d9e
oscar <ordc120@gmail.com>
2024-07-12 13:17:33 +0200

    pagina que verifica que el usuario fue creado correctamente

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import AdminPage from './pages/AdminPage';
 import Home from './pages/Home';
 import ResetPass from './components/resetPass';
+import ActivationSuccessPage from './pages/ActivationSuccessPage';
 import LoginFormPage from './pages/LoginFormPage'
 
 import RegisterFormPage from './pages/RegisterFormPage';
@@ -13,6 +14,7 @@ const App = () => {
       <Route path="/admin/:eventId?" element={<AdminPage />} />
       <Route path="/register" element={<RegisterFormPage />} />
       <Route path="/reset-password" element={<ResetPass />} />
+      <Route path="/users/validate" element={<ActivationSuccessPage />} />
       <Route path="/users/login" element={<LoginFormPage />} />
     </Routes>
   );

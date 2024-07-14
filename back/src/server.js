dd8ce7afd577fe23c697cbab57e9934f32bd634d
Juan Ignacio Macias Perez <juanmaciasperez89@gmail.com>
2024-07-14 19:06:45 +0100

    Cambio ruta de endpoint para editar eventos, hotfix de parseo de variable de entorno para PUBLIC_FOLDER, coleccion postman actualizada

--- back/src/server.js
+++ back/src/server.js
@@ -1,29 +1,29 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
-import routes from "./routes/index.js";
-import path from "path";
-import "dotenv/config";
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import routes from './routes/index.js';
+import path from 'path';
+import 'dotenv/config';
 
-import { errorHandler, notFoundHandler } from "./middlewares/index.js";
-import fileUpload from "express-fileupload";
+import { errorHandler, notFoundHandler } from './middlewares/index.js';
+import fileUpload from 'express-fileupload';
 
 const server = express();
 
 // Recursos estáticos
 const { UPLOADS_DIR } = process.env;
-const PUBLIC_FOLDER = path.join(process.cwd(), UPLOADS_DIR);
+const PUBLIC_FOLDER = path.join(process.cwd(), String(UPLOADS_DIR));
 server.use(express.static(PUBLIC_FOLDER));
 
-server.use(morgan("dev"));
+server.use(morgan('dev'));
 server.use(cors());
 server.use(express.json()); //*como me hacia falta, ya lo pongo
 
 server.use(fileUpload());
 
 // Llamado a rutas
-server.get("/", (req, res) => {
-  res.send("El servidor está funcionando correctamente");
+server.get('/', (req, res) => {
+    res.send('El servidor está funcionando correctamente');
 });
 
 server.use(routes);

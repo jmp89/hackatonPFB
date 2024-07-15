e103c92b16846f3423d646b1418e36a31bf5b4fa
George Nicu <gnicuu@gmail.com>
2024-07-15 11:19:00 +0100

    Recuperación de contraseñas

--- front/src/components/resetPass.jsx
+++ front/src/components/resetPass.jsx
@@ -6,6 +6,7 @@ const ResetPass = () => {
     const [email, setEmail] = useState('');
     const [recoverPassCode, setRecoverPassCode] = useState('');
     const [newPassword, setNewPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
     const [message, setMessage] = useState('');
     const [step, setStep] = useState(1);
 
@@ -22,69 +23,92 @@ const ResetPass = () => {
 
     const handlePasswordReset = async (e) => {
         e.preventDefault();
+        if (newPassword !== confirmPassword) {
+            setMessage('Las contraseñas no coinciden.');
+            return;
+        }
         try {
-            const response = await resetPassword(email, recoverPassCode, newPassword);
+            const response = await resetPassword({
+                email,
+                recoverPassCode,
+                newPassword,
+                repeatNewPassword: confirmPassword,
+            });
             setMessage(response.message || 'Contraseña cambiada correctamente.');
-            setStep(3); 
+            setStep(3);
         } catch (error) {
-            setMessage(error.message);
+            setMessage(`Error en la solicitud: ${error.message}`);
         }
     };
 
     return (
         <main className="flex items-center justify-center min-h-screen bg-gray-100 px-4">
-            <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-md">
+            <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm">
                 <h2 className="text-2xl font-bold text-center mb-6">Recuperar Contraseña</h2>
                 {step === 1 && (
                     <form onSubmit={handleEmailSubmit}>
-                        <div>
-                            <label className="block text-lg font-medium mb-2">Email</label>
+                        <div className="mb-4 flex flex-col items-center">
+                            <label className="block text-lg font-medium mb-2 text-center">Email</label>
                             <input
                                 type="email"
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
                                 required
-                                className="w-full border rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
+                                className="w-full border rounded-lg focus:outline-none focus:ring-2 focus:ring-black px-4 py-2"
+                                placeholder="Escribe tu email"
                             />
                         </div>
-                        <button type="submit" className="w-full bg-black text-white py-1 my-5 rounded-lg font-bold text-lg mb-4">
+                        <button type="submit" className="w-full bg-black text-white py-2 rounded-lg font-bold text-lg">
                             Enviar código de verificación
                         </button>
                     </form>
                 )}
                 {step === 2 && (
                     <form onSubmit={handlePasswordReset}>
-                        <div>
-                            <label className="block text-lg font-medium mb-2">Código de verificación:</label>
+                        <div className="mb-4 flex flex-col items-center">
+                            <label className="block text-lg font-medium mb-2 text-center">Código de verificación</label>
                             <input
                                 type="text"
                                 value={recoverPassCode}
                                 onChange={(e) => setRecoverPassCode(e.target.value)}
                                 required
-                                className="w-full border rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
+                                className="w-full border rounded-lg focus:outline-none focus:ring-2 focus:ring-black px-4 py-2"
+                                placeholder="Escribe el código de verificación"
                             />
                         </div>
-                        <div>
-                            <label className="block text-lg font-medium mb-2">Nueva Contraseña:</label>
+                        <div className="mb-4 flex flex-col items-center">
+                            <label className="block text-lg font-medium mb-2 text-center">Nueva Contraseña</label>
                             <input
                                 type="password"
                                 value={newPassword}
                                 onChange={(e) => setNewPassword(e.target.value)}
                                 required
-                                className="w-full border rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
+                                className="w-full border rounded-lg focus:outline-none focus:ring-2 focus:ring-black px-4 py-2"
+                                placeholder="Escribe tu nueva contraseña"
                             />
                         </div>
-                        <button type="submit" className="w-full bg-black text-white py-1 my-5 rounded-lg font-bold text-lg mb-4">
+                        <div className="mb-4 flex flex-col items-center">
+                            <label className="block text-lg font-medium mb-2 text-center">Repite la Contraseña</label>
+                            <input
+                                type="password"
+                                value={confirmPassword}
+                                onChange={(e) => setConfirmPassword(e.target.value)}
+                                required
+                                className="w-full border rounded-lg focus:outline-none focus:ring-2 focus:ring-black px-4 py-2"
+                                placeholder="Repite tu nueva contraseña"
+                            />
+                        </div>
+                        <button type="submit" className="w-full bg-black text-white py-2 rounded-lg font-bold text-lg">
                             Restablecer Contraseña
                         </button>
                     </form>
                 )}
                 {step === 3 && (
-                    <p className="w-full bg-black text-white py-1 my-5 rounded-lg font-bold text-lg mb-4">
+                    <p className="bg-green-500 text-white py-2 rounded-lg font-bold text-lg text-center">
                         Contraseña cambiada correctamente.
                     </p>
                 )}
-                {message && <p>{message}</p>}
+                {message && <p className="text-center text-red-500 mt-4">{message}</p>}
             </div>
         </main>
     );
--- front/src/services/resetPasswordServices.js
+++ front/src/services/resetPasswordServices.js
@@ -1,26 +1,27 @@
-export const resetPassword = async (email, recoverPassCode, newPassword) => {
-  try {
-      const response = await fetch('http://localhost:3001/users/reset-password', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-              email,
-              recoverPassCode,
-              newPassword,
-          }),
-      });
-
-      if (!response.ok) {
-          throw new Error('Error en la solicitud: ' + response.statusText);
-      }
-
-      const data = await response.json();
-      console.log(data);
-      return data;
-  } catch (error) {
-      console.error('Error sending verification code:', error);
-      throw error;
-  }
-};
+export const resetPassword = async ({ email, recoverPassCode, newPassword, repeatNewPassword }) => {
+    try {
+        const response = await fetch('http://localhost:3001/users/reset-password', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email,
+                recoverPassCode,
+                newPassword,
+                repeatNewPassword,
+            }),
+        });
+  
+        if (!response.ok) {
+            throw new Error('Error en la solicitud: ' + response.statusText);
+        }
+  
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error en el restablecimiento de contraseña:', error);
+        throw error;
+    }
+  };
+  

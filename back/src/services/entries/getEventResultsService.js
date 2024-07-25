8f0041a7b8d1eaa663d1ec6225c75af259c9cc50
Adrián Conde Pérez <acp641991@gmail.com>
2024-07-25 12:33:18 +0200

    arrays tech/thems on finished-events and event-results

--- back/src/services/entries/getEventResultsService.js
+++ back/src/services/entries/getEventResultsService.js
@@ -3,9 +3,9 @@ import getPool from '../../database/getPool.js';
 const getEventResultsService = async () => {
     const pool = await getPool();
 
-    const finishedEvents = await pool.query(`
+    const [results] = await pool.query(`
         SELECT
-        e.id AS event_id,
+        e.id,
         e.name AS event_name,
         t.name AS technologies,
         th.name AS thematics,
@@ -32,7 +32,44 @@ const getEventResultsService = async () => {
         ORDER BY e.finish_date
     `);
 
-    return finishedEvents[0];
+    const eventsMap = new Map();
+
+    for (const row of results) {
+        if (!eventsMap.has(row.id)) {
+            eventsMap.set(row.id, {
+                id: row.id,
+                name: row.name,
+                technologies: new Set(),
+                thematics: new Set(),
+                online_on_site: row.online_on_site,
+                location: row.location,
+                organizer: row.organizer,
+                start_date: row.start_date,
+                finish_date: row.finish_date,
+                start_time: row.start_time,
+                finish_time: row.finish_time
+            });
+        };
+        
+        const event = eventsMap.get(row.id);
+
+        if (row.technologies) {
+            event.technologies.add(row.technologies);
+        };
+
+        if (row.thematics) {
+
+            event.thematics.add(row.thematics);
+        };
+    };
+
+    const finalEventsList = Array.from(eventsMap.values()).map(event => ({
+        ...event,
+        technologies: Array.from(event.technologies),
+        thematics: Array.from(event.thematics)
+    }));
+
+    return finalEventsList;
 };
 
 export default getEventResultsService;

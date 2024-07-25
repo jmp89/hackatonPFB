9c2bfd16ca0ed98c8122eef2c59055ab6169ff7b
Adrián Conde Pérez <acp641991@gmail.com>
2024-07-25 11:31:03 +0200

    last update on my-events

--- back/src/services/users/getMyEventsListService.js
+++ back/src/services/users/getMyEventsListService.js
@@ -4,7 +4,7 @@ const getMyEventsListService = async (queryUserId) => {
 
     const pool = await getPool();
 
-    const [myEvents] = await pool.query(`
+    const [results] = await pool.query(`
         SELECT e.id,
         e.name,
         t.name AS technologies,
@@ -26,7 +26,44 @@ const getMyEventsListService = async (queryUserId) => {
    `, [queryUserId]
     );
 
-    return myEvents;
+    const eventsMap = new Map();
+
+    for (const row of results) {
+        if (!eventsMap.has(row.id)) {
+            eventsMap.set(row.id, {
+                id: row.id,
+                name: row.name,
+                technologies: new Set(),
+                thematics: new Set(),
+                online_on_site: row.online_on_site,
+                location: row.location,
+                organizer: row.organizer,
+                start_date: row.start_date,
+                finish_date: row.finish_date,
+                start_time: row.start_time,
+                finish_time: row.finish_time
+            });
+        };
+        
+        const event = eventsMap.get(row.id);
+
+        if (row.technologies) {
+            event.technologies.add(row.technologies);
+        };
+
+        if (row.thematics) {
+
+            event.thematics.add(row.thematics);
+        };
+    };
+
+    const finalEventsList = Array.from(eventsMap.values()).map(event => ({
+        ...event,
+        technologies: Array.from(event.technologies),
+        thematics: Array.from(event.thematics)
+    }));
+
+    return finalEventsList;
 };
 
 export default getMyEventsListService;

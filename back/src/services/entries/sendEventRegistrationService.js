be5b70d17a8eb831e513a02bf6bf0eb530bb4702
George Ionut Nicu <gnicuu@gmail.com>
2024-07-31 12:44:32 +0100

    sendEvent

--- back/src/services/entries/sendEventRegistrationService.js
+++ back/src/services/entries/sendEventRegistrationService.js
@@ -24,19 +24,14 @@ const sendEventRegistrationMailService = async (eventID, id, eventCode) => {
     const emailSubject = `Confirma tu inscripción en ${eventInfo.name}`;
 
     const emailBody = `
-        Gracias por enviarnos tu solicitud, a continuación te mostramos un poco de qué va este Hackathon con más detalle:
-
-        
-
-        
+        <h1></h1>Gracias por enviarnos tu solicitud, a continuación te mostramos un poco de qué va este Hackathon con más detalle:
+        </h1>
         <h2>¿De qué va ${eventInfo.name}?</h2>
         
         <p>${eventInfo.description}</p>
         
-        Haz click en el siguiente botón para confirmar la inscripción.
-
         
-        <a href="http://localhost:3001/event/confirm/${eventCode}" style="color: #fff; background-color: #000; padding: 15px 25px; border-radius: 10px; margin: 0 25px">¡Apúntate!</a>
+        <a href="http://localhost:5173/event/confirm/${eventCode}" style="color: #fff; background-color: #000; padding: 15px 25px; border-radius: 10px; margin: 0 25px">Haz click aqui para confirmar la inscripción.</a>
 
         <hr />
         Hecho con ❤ por el equipo de Hackathon

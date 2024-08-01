717fcaa7ae68f69000ed2ce37c8b2da5f9fc5ad0
George Ionut Nicu <gnicuu@gmail.com>
2024-08-01 14:40:04 +0100

    actualizacionHome

--- front/src/components/Home.jsx
+++ front/src/components/Home.jsx
@@ -44,14 +44,12 @@ const Home = () => {
     };
 
     return (
-        <main className="container mx-auto px-4 py-8">
-            <section className="text-center">
-                <h1 className="text-4xl font-bold mt-10">
-                    ¡BIENVENIDOS A HACKAVERSE!
-                </h1>
-                <p className="text-xl font-medium mt-4">
-                    LA MEJOR PLATAFORMA DE HACKATONES
-                </p>
+        <main className="w-full h-full px-0 py-8 text-center">
+            <section className="py-8 h-56 bg-zinc-800 text-white sd:h-auto">
+                <div className="container mx-auto px-4 md:px-6">
+                    <h1 className="text-4xl font-bold mt-10">¡BIENVENIDOS A HACKAVERSE!</h1>
+                    <p className="text-xl font-medium mt-4">LA MEJOR PLATAFORMA DE HACKATONES</p>
+                </div>
             </section>
 
             <section className="mt-10 text-center">
@@ -61,34 +59,32 @@ const Home = () => {
                 {error ? (
                     <p className="text-red-500">{error}</p>
                 ) : (
-                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 w-full max-w-6xl mx-auto ">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 w-full max-w-6xl mx-auto">
                         {Array.isArray(events) &&
                             events.map((event, index) => (
                                 <article
-                                key={event.id || index}
-                                className="border p-4 rounded-lg shadow-md text-center cursor-pointer transition-transform transform hover:scale-105 hover:shadow-xl max-w-[800px] max-h-[800px] flex"
-                                onClick={() => handleEventClick(event.id)}
-                            >
-                                <div className="w-1/3 flex-shrink-0 h-full">
-                                    <img
-                                        src={API_URL + event.image}
-                                        alt="event-image"
-                                        className="w-full h-full object-cover rounded-lg"
-                                    />
-                                </div>
-                                <div className="w-2/3 pl-4 flex flex-col justify-between">
-                                    <h3 className="text-xl font-bold">{event.name}</h3>
-                                    <p>Organizador: {event.organizer_name}</p>
-                                    <p> Inicio: {event.start_date.split('T')[0]} </p>
-                                    <p> Fin: {event.finish_date.split('T')[0]}</p>
-                                    {/* <p>Localidad: {event.location}</p> */}
-                                    <p>{event.online_on_site !== 'on_site' ? 'Online' : `Ciudad: ${event.location}`}</p>
-                                    <p>Tecnología: {event.technologies}</p>
-                                    <p>Temática: {event.thematics}</p>
-                                </div>
-                            </article>
-                            
-                            
+                                    key={event.id || index}
+                                    className="border p-4 rounded-lg shadow-md text-center cursor-pointer transition-transform transform hover:scale-105 hover:shadow-xl max-w-[800px] max-h-[800px] flex"
+                                    onClick={() => handleEventClick(event.id)}
+                                >
+                                    <div className="w-1/3 flex-shrink-0 h-full">
+                                        <img
+                                            src={API_URL + event.image}
+                                            alt="event-image"
+                                            className="w-full h-full object-cover rounded-lg"
+                                        />
+                                    </div>
+                                    <div className="w-2/3 pl-4 flex flex-col justify-between">
+                                        <h3 className="text-xl font-bold">{event.name}</h3>
+                                        <p>Organizador: {event.organizer_name}</p>
+                                        <p> Inicio: {event.start_date.split('T')[0]} </p>
+                                        <p> Fin: {event.finish_date.split('T')[0]}</p>
+                                        {/* <p>Localidad: {event.location}</p> */}
+                                        <p>{event.online_on_site !== 'on_site' ? 'Online' : `Ciudad: ${event.location}`}</p>
+                                        <p>Tecnología: {event.technologies}</p>
+                                        <p>Temática: {event.thematics}</p>
+                                    </div>
+                                </article>
                             ))}
                     </div>
                 )}
